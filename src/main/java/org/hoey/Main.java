061734b3d87868380c962a2dcbd36ce731736e40
liuhuijun <liuhuijun2017@gmail.com>
2024-06-09 16:42:09 +0800

    init

---
+++ src/main/java/org/hoey/Main.java
@@ -0,0 +1,13 @@
+package org.hoey;
+
+/**
+ * TODO
+ *
+ * @author liuhuijun
+ * @since 2024/6/8 16:52
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
